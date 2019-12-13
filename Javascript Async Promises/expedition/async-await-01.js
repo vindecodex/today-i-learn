fa123dd025687252e2847a19d45cac70f7731499
Vincent Villaluna <villalunavincent@gmail.com>
2019-12-13 11:18:34 +0800

    added comments

--- Javascript Async Promises/expedition/async-await-01.js
+++ Javascript Async Promises/expedition/async-await-01.js
@@ -1,16 +1,23 @@
+// It accepts value to be count
 async function generator(val) {
   let generated=0
    for(let i = 0; i < val; i++) {
+     // it increments and used to be returned
      generated++
    }
+  // it will took time to return for value if big
    return Promise.resolve(generated)
 }
 async function double(value){
+  // val will use the returned value of our async so we use await to pause and wait for the generator function to return a value
   let val = await generator(value)
+  // I just double the val to: val + val as what the function name
   val += val 
   return val 
 }
+// I also did some synchronous task here
 for(let i = 0; i < 8000; i++) {
   console.log(i, "////////")
 }
+// I pass big number to take time to process and also to make it look more realistic
 double(800000000).then(data => console.log("Answer is: ", data))
