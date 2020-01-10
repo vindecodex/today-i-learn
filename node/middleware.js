a7b9ccaa29380f416afa896995b4409ef334e116
Vincent Villaluna <villalunavincent@gmail.com>
2020-01-10 12:49:50 +0800

    creating middleware

---
+++ node/middleware.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = new express();
+
+/* Any of this params name can be change, but it remains 1st argument will be request 2nd is response, last is next method
+ * Everytime there is a request middleware will trigger
+ * We can actually create multiple middlewares
+ */
+// Middleware
+app.use((req, res, next) => {
+  // we can use request and response properties/attributes
+  console.log('Hello');
+  // after all next fn must be called so that it wont left hangging the request
+  next();
+});
+
+// Handlers
+const getData = (req,res) => {
+  res.status(200).json({
+    data: data
+  })
+}
+
+// Route
+/*
+ * Everytime we request a console.log('Hello will always trigger');
+ */
+app.get('/sample', getData);
+
